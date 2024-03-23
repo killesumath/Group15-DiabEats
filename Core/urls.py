b52a601d89d8e89db583a7ee9f4f03e99ec9cc33
MelsaBasnet <melishabasnet170@gmail.com>
2024-03-23 14:10:19 -0400

    User signup and authentication created

--- Core/urls.py
+++ Core/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 
 from DiabEats import settings
 from . import views
+from .views import *
+from .views import index
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login/', views.login, name='login'),
+    path('login/', login_view, name='login'),
+    path('signup/', signup_post, name='signup')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_URL)
